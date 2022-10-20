12a71e05791110cb55e281415ecf31850ae7589e
Ruan E. Formigoni <ruanformigoni@gmail.com>
2022-10-20 15:25:06 -0300

    Included appimage deploy

---
+++ deploy/deploy.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+######################################################################
+# @author      : Ruan E. Formigoni (ruanformigoni@gmail.com)
+# @file        : deploy
+######################################################################
+
+set -e
+
+# Create build dir
+mkdir -p build && cd build
+
+# Create appdir
+mkdir -p AppDir/usr/bin
+
+# Copy files
+cp -r ../src/* AppDir/usr/bin
+cp -r ../doc/agape.png AppDir/
+
+for i in AppDir/usr/bin/*; do
+  echo "$i"
+  chmod +x "$i"
+done
+
+# Create runner script
+{ sed -E 's/^\s+://' | tee AppDir/AppRun; } <<-END
+  :#!/usr/bin/env bash
+  :
+  :\$APPDIR/usr/bin/main.sh \$@
+END
+chmod +x AppDir/AppRun
+
+# Create desktop entry
+{ sed -E 's/^\s+://' | tee AppDir/agape.desktop; } <<-END
+  :[Desktop Entry]
+  :Name=Agape
+  :Exec=/usr/bin/bash
+  :Icon=agape
+  :Type=Application
+  :Categories=Utility;
+END
+
+# Download appimagetool
+[ ! -f "appimagetool" ] && wget -q --show-progress --progress=bar:noscroll -O appimagetool https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+
+chmod +x appimagetool
+
+# Extract
+./appimagetool --appimage-extract
+
+# Package
+ARCH=x86_64 ./squashfs-root/AppRun AppDir
