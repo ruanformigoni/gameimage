b0e6d4c10b6ba0d55d75ee0f954bee97b8627fab
Ruan E. Formigoni <ruanformigoni@gmail.com>
2023-02-15 03:58:01 -0300

    Fix for unionfs hangs

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -23,11 +23,10 @@ mkdir -p AppDir/usr/bin
 # Fetch & Compile uniofs-fuse
 [ -d "unionfs-fuse" ] || git clone https://github.com/rpodgorny/unionfs-fuse.git
 cd unionfs-fuse
-cmake -H. -Bbuild
-cmake --build build
-cd ..
+# Workaround for #133 in https://github.com/rpodgorny/unionfs-fuse
+git reset --hard 453f588a0d227e31730244f45fce3adb0ecbe95d
+cmake -H. -Bbuild && cmake --build build && cd ..
 cp unionfs-fuse/build/src/unionfs AppDir/usr/bin
-rm -rf unionfs-fuse
 
 # Fetch yq
 wget -q --show-progress --progress=dot:mega https://github.com/mikefarah/yq/releases/download/v4.30.7/yq_linux_amd64.tar.gz -O - | tar xz
