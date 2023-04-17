770715497c96bb58ddd091c5c345554ad447285c
Ruan E. Formigoni <ruanformigoni@gmail.com>
2023-04-16 23:40:36 -0300

    Static binaries GUIs

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,53 +9,48 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Create build dir and appdir
+mkdir -p build/AppDir/usr/bin
+
 # Compile gui
 cd gui/wizard && cargo build --release
-
 cd "$(dirname "$SCRIPT_DIR")"
+staticx ./gui/wizard/target/release/gameimage-install-gui build/AppDir/usr/bin/gui
 
 # Compile launcher
 cd gui/launcher && cargo build --release
-
 cd "$(dirname "$SCRIPT_DIR")"
-
-# Create build dir
-mkdir -p build && cd build
-
-# Create appdir
-mkdir -p AppDir/usr/bin
+staticx -l"/usr/lib/libappindicator3.so.1" ./gui/launcher/target/release/gameimage-launcher build/AppDir/usr/bin/launcher
 
 # Fetch unionfs
 wget -q --show-progress --progress=dot:mega https://github.com/ruanformigoni/unionfs-fuse/releases/download/ebac73a/unionfs
-mv -f unionfs AppDir/usr/bin
+mv -f unionfs build/AppDir/usr/bin
 
 # Fetch yq
 wget -q --show-progress --progress=dot:mega https://github.com/mikefarah/yq/releases/download/v4.30.7/yq_linux_amd64.tar.gz -O - | tar xz
 rm yq.1
 mv yq_linux_amd64 yq
-mv yq AppDir/usr/bin
+mv yq build/AppDir/usr/bin
 
 # Copy files
-cp -r ../src/* AppDir/usr/bin
-cp -r ../doc/gameimage.png AppDir/
-cp -r ../gui/wizard/target/release/gameimage-install-gui AppDir/usr/bin/gui
-cp -r ../gui/launcher/target/release/gameimage-launcher AppDir/usr/bin/launcher
+cp -r ./src/* build/AppDir/usr/bin
+cp    ./doc/gameimage.png build/AppDir/
 
-for i in AppDir/usr/bin/*; do
+for i in build/AppDir/usr/bin/*; do
   echo "$i"
   chmod +x "$i"
 done
 
 # Create runner script
-{ sed -E 's/^\s+://' | tee AppDir/AppRun; } <<-END
+{ sed -E 's/^\s+://' | tee build/AppDir/AppRun; } <<-END
   :#!/usr/bin/env bash
   :
   :\$APPDIR/usr/bin/main.sh "\$@"
 END
-chmod +x AppDir/AppRun
+chmod +x build/AppDir/AppRun
 
 # Create desktop entry
-{ sed -E 's/^\s+://' | tee AppDir/gameimage.desktop; } <<-END
+{ sed -E 's/^\s+://' | tee build/AppDir/gameimage.desktop; } <<-END
   :[Desktop Entry]
   :Name=GameImage
   :Exec=/usr/bin/bash
@@ -63,6 +58,9 @@ chmod +x AppDir/AppRun
   :Type=Application
   :Categories=Utility;
 END
+
+# Enter build dir
+cd build
 
 # Download appimagetool
 [ ! -f "appimagetool" ] && wget -q --show-progress --progress=bar:noscroll -O appimagetool https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
