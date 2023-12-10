cf7ab3bf651a3ceda6e74454cd67ec0bb2a94e11
Ruan E. Formigoni <ruanformigoni@gmail.com>
2023-12-09 21:10:57 -0300

    Fix premature removal of opt.dwarfs

--- src/wine.sh
+++ src/wine.sh
@@ -45,12 +45,10 @@ function _fetch_wine()
         _fetch "wine" "https://gitlab.com/api/v4/projects/45732205/packages/generic/wine/continuous/base-arch.fim"
         _fetch "opt.dwarfs" "$url"
         chmod +x ./wine
-        # Remove if previous attempt failed
-        rm -f opt.dwarfs
         # Merge flatimage with wine binaries
         ./wine fim-include-path ./opt.dwarfs "/opt.dwarfs"
         # Remove downloaded wine binaries
-        rm opt.dwarfs
+        rm -f ./opt.dwarfs
       else
         _fetch "wine" "$url"
       fi
