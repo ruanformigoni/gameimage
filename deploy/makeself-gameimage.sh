9857a2910fc4a73819d4a4cfa0f1b8309a13095e
Ruan E. Formigoni <ruanformigoni@gmail.com>
2023-12-22 06:14:51 -0300

    Switch to makeself package

---
+++ deploy/makeself-gameimage.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+
+######################################################################
+# @author      : Ruan E. Formigoni (ruanformigoni@gmail.com)
+# @file        : deploy
+######################################################################
+
+set -e
+
+#
+# Setup environment
+#
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Reference source dir
+SRC_DIR="$(dirname "$SCRIPT_DIR")"
+cd "$SRC_DIR"
+
+# Create build dir and appdir
+BUILD_DIR="$SRC_DIR/build"
+mkdir -p "$BUILD_DIR"
+
+# Binaries location on package
+BIN_DIR="$BUILD_DIR/app/bin"
+mkdir -p "$BIN_DIR"
+
+#
+# Compile GUI
+#
+
+# Compile wizard, patch, and package with makeself
+docker build . -t wizard:alpine -f deploy/Dockerfile.alpine.wizard
+docker run -it --rm -v "$(pwd)":/workdir wizard:alpine cp /dist/wizard /workdir
+cp ./wizard "$BIN_DIR"
+rm -f ./wizard
+
+# Launcher does not need to be static since it runs inside the arch container
+docker build . -t launcher:alpine -f deploy/Dockerfile.arch.launcher
+docker run -it --rm -v "$(pwd)":/workdir launcher:alpine cp /dist/launcher /workdir
+cp ./launcher "$BIN_DIR"
+rm -f ./launcher
+
+#
+# Fetch tools
+#
+
+function _fetch()
+{
+  local link="$1"
+  local out="$2"
+  local file="$3"
+  if [[ "$link" =~ .tar.(xz|gz) ]]; then
+    wget -q --show-progress --progress=dot:mega "$link" -O - | tar xz "$file" -O > "$out"
+  else
+    wget -q --show-progress --progress=dot:mega "$link" -O "$out"
+  fi
+  chmod +x "$out"
+}
+
+# Fetch unionfs
+_fetch "https://github.com/ruanformigoni/unionfs-fuse/releases/download/ebac73a/unionfs" "$BIN_DIR"/unionfs
+
+# Fetch overlayfs
+_fetch "https://github.com/ruanformigoni/fuse-overlayfs/releases/download/af507a2/fuse-overlayfs-x86_64" "$BIN_DIR"/overlayfs
+
+# Fetch yq
+_fetch "https://github.com/mikefarah/yq/releases/download/v4.30.7/yq_linux_amd64.tar.gz" "$BIN_DIR/yq" "./yq_linux_amd64"
+
+# Fetch jq
+_fetch "https://github.com/jqlang/jq/releases/download/jq-1.7/jq-linux-amd64" "$BIN_DIR"/jq
+
+# Fetch 7zz
+_fetch "https://github.com/ruanformigoni/7zip_static/releases/download/ed1f3df/7zz" "$BIN_DIR"/7zz
+
+# Fetch busybox
+_fetch "https://www.busybox.net/downloads/binaries/1.35.0-x86_64-linux-musl/busybox" "$BIN_DIR"/busybox
+
+# Fetch fd
+_fetch "https://github.com/sharkdp/fd/releases/download/v8.7.1/fd-v8.7.1-x86_64-unknown-linux-musl.tar.gz" "$BIN_DIR"/fd "fd-v8.7.1-x86_64-unknown-linux-musl/fd"
+
+# Fetch aria2
+_fetch "https://github.com/ruanformigoni/aria2-static-musl/releases/download/2d7f402/aria2c" "$BIN_DIR"/aria2c
+
+# Fetch bash
+_fetch "https://github.com/ruanformigoni/bash-static/releases/download/8ba11cd/bash-x86_64" "$BIN_DIR"/bash
+
+# Fetch coreutils
+wget -q --show-progress --progress=dot:mega "https://github.com/ruanformigoni/coreutils-static/releases/download/d7f4cd2/coreutils-x86_64.tar.xz"
+tar -xf "coreutils-x86_64.tar.xz" -C"$BIN_DIR" --strip-components=1
+rm "coreutils-x86_64.tar.xz"
+
+# Fetch gnu tools
+cp /home/ruan/Repositories/gnu-static-musl/dist/* "$BIN_DIR"
+
+# Copy files
+cp -r ./src/* "$BUILD_DIR"/app/bin
+cp    ./doc/gameimage.png "$BUILD_DIR"/app
+
+for i in "$BUILD_DIR"/app/bin/*; do
+  echo "$i"
+  chmod +x "$i"
+done
+
+#
+# Build
+#
+
+cd "$BUILD_DIR"
+
+# Create runner script
+{ sed -E 's/^\s+://' | tee "$BUILD_DIR"/app/start.sh; } <<-"END"
+  :#!/bin/sh
+  :
+  :PATH_SCRIPT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+  :
+  :PATH="$PATH_SCRIPT:$PATH"
+  :PATH="$PATH_SCRIPT/bin:$PATH"
+  :PATH="/tmp/gameimage/bin:$PATH"
+  :
+  :mkdir -p /tmp/gameimage/bin
+  :cp ./bin/bash /tmp/gameimage/bin
+  :
+  :main.sh "$@"
+END
+chmod +x "$BUILD_DIR"/app/start.sh
+
+# Package
+mkdir -p tools
+wget -O ./tools/makeself "https://github.com/megastep/makeself/releases/download/release-2.5.0/makeself-2.5.0.run"
+chmod +x ./tools/makeself && ./tools/makeself --target ./tools && chmod +x ./tools/*.sh
+./tools/makeself.sh --xz "$BUILD_DIR/app" "gameimage.run" "Gameimage" './start.sh'
