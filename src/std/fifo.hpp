008497728c86a99a05f3f47ce877a60f876dd2bb
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-04-09 22:01:34 -0300

    Rework fifo for RAII

--- src/std/fifo.hpp
+++ src/std/fifo.hpp
@@ -9,57 +9,86 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <cstdlib>
+#include <fcntl.h>
 #include <filesystem>
+
+#include "../std/filesystem.hpp"
 
 #include "../common.hpp"
 
-namespace fifo
+namespace ns_fifo
 {
 
 namespace fs = std::filesystem;
 
-// create() {{{
-// Create a FIFO for writing progress data
-inline void create(const char* str_name)
+// class Fifo {{{
+class Fifo
 {
-  // Check if already exists
-  if (struct stat buf; stat(str_name, &buf) == 0 && S_ISFIFO(buf.st_mode) )
+  private:
+    int m_fd;
+    fs::path m_path_file_fifo;
+  
+  public:
+    template<ns_concept::AsString T>
+    Fifo(T&& t);
+    ~Fifo();
+    template<ns_concept::AsString T>
+    void push(T&& t);
+}; // class: Fifo }}}
+
+// Fifo::Fifo {{{
+template<ns_concept::AsString T>
+Fifo::Fifo(T&& t)
+{
+  m_path_file_fifo = ns_string::to_string(t);
+  auto cstr_path_file_fifo = m_path_file_fifo.c_str();
+
+  // Remove the FIFO if it already exists
+  if (struct stat buf; stat(cstr_path_file_fifo, &buf) == 0 && S_ISFIFO(buf.st_mode) )
   {
-    return;
+    unlink(cstr_path_file_fifo);
   } // if
 
   // Create upper dirs
-  auto path = fs::path(str_name);
-  auto path_parent = path.parent_path();
-  if ( ! fs::exists(path_parent) )
+  if ( not ns_fs::ns_path::dir_create<false>(fs::path(cstr_path_file_fifo).parent_path())._bool )
   {
-    if ( ! fs::create_directories(path_parent) )
-    {
-      "Failed to create upper directories for '{}'"_throw(str_name);
-    }
-  }
-
-  // Remove the FIFO if it already exists
-  unlink(str_name);
+    "Failed to create upper directories for '{}'"_throw(cstr_path_file_fifo);
+  } // if
 
   // Create a new FIFO
-  if (mkfifo(str_name, 0666) != 0) {
+  if (mkfifo(cstr_path_file_fifo, 0666) != 0)
+  {
     perror("mkfifo");
-    "mkfifo failed for '{}'"_throw(str_name);
-  }
-} // create() }}}
+    "mkfifo failed for '{}'"_throw(cstr_path_file_fifo);
+  } // if
+
+  // Open fifo as writer
+  if (m_fd = open(cstr_path_file_fifo, O_WRONLY | O_NONBLOCK); m_fd == -1 )
+  {
+    "Could not open fifo file descriptor for writing"_throw();
+    return;
+  } // if
+} // }}}
+
+// Fifo::~Fifo {{{
+inline Fifo::~Fifo()
+{
+  close(m_fd);
+  fs::remove(m_path_file_fifo);
+} // }}}
 
 // push() {{{
-inline void push(const char* str_name, std::string data)
+template<ns_concept::AsString T>
+void Fifo::push(T&& t)
 {
-  if (int fd = open(str_name, O_WRONLY | O_NONBLOCK); fd != -1)
+  auto data = ns_string::to_string(t);
+  auto count_bytes_written = write(m_fd, data.c_str(), data.size());
+  if ( count_bytes_written == -1 )
   {
-    write(fd, data.c_str(), data.size());
-    close(fd);
+    ns_log::write('e', "Failed to write data '", data, "' to fifo");
   } // if
 } // push() }}}
 
-} // namespace fifo
+} // namespace ns_fifo
 
 /* vim: set expandtab fdm=marker ts=2 sw=2 tw=100 et :*/
