06d758aff5bb3a48d40672db00303f2fb3812de2
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-11-05 09:29:14 -0300

    Refactor redraw()

--- gui/wizard/src/main.rs
+++ gui/wizard/src/main.rs
@@ -108,181 +108,50 @@ fn redraw(&mut self, msg : Msg)
 
   match msg
   {
-    //
     // Common
-    //
-    Msg::DrawFinish =>
-    {
-      frame::finish::finish(self.tx, "Thank You for Using GameImage!");
-    }
-    Msg::DrawWelcome =>
-    {
-      frame::welcome::welcome(self.tx, "Welcome to GameImage");
-    }
-    Msg::DrawPlatform =>
-    {
-      frame::platform::platform(self.tx, "Select a Platform");
-    }
-    Msg::DrawCreator =>
-    {
-      frame::creator::creator(self.tx, "Create Packages to Include in the Image");
-    }
-    Msg::DrawDesktop =>
-    {
-      frame::desktop::desktop(self.tx, "Select the Desktop Icon");
-    }
-
-    //
+    Msg::DrawFinish => frame::finish::finish(self.tx, "Thank You for Using GameImage!"),
+    Msg::DrawWelcome => frame::welcome::welcome(self.tx, "Welcome to GameImage"),
+    Msg::DrawPlatform => frame::platform::platform(self.tx, "Select a Platform"),
+    Msg::DrawCreator => frame::creator::creator(self.tx, "Create Packages to Include in the Image"),
+    Msg::DrawDesktop => frame::desktop::desktop(self.tx, "Select the Desktop Icon"),
     // Linux
-    //
-    Msg::DrawLinuxName =>
-    {
-      wizard::linux::name(self.tx, "Select the Application Name");
-    }
-    Msg::DrawLinuxIcon =>
-    {
-      wizard::linux::icon(self.tx, "Select the Application Icon");
-    }
-    Msg::DrawLinuxMethod =>
-    {
-      wizard::linux::method(self.tx, "Select How to Install the Application");
-    }
-    Msg::DrawLinuxRom =>
-    {
-      wizard::linux::rom(self.tx, "Install the Application");
-    }
-    Msg::DrawLinuxDefault =>
-    {
-      wizard::linux::default(self.tx, "Select the Main Binary");
-    }
-    Msg::DrawLinuxTest =>
-    {
-      wizard::linux::test(self.tx, "Test the Application");
-    }
-    Msg::DrawLinuxCompress =>
-    {
-      wizard::linux::compress(self.tx, "Compress the Created Package");
-    }
-
-    //
+    Msg::DrawLinuxName => wizard::linux::name(self.tx, "Select the Application Name"),
+    Msg::DrawLinuxIcon => wizard::linux::icon(self.tx, "Select the Application Icon"),
+    Msg::DrawLinuxMethod => wizard::linux::method(self.tx, "Select How to Install the Application"),
+    Msg::DrawLinuxRom => wizard::linux::rom(self.tx, "Install the Application"),
+    Msg::DrawLinuxDefault => wizard::linux::default(self.tx, "Select the Main Binary"),
+    Msg::DrawLinuxTest => wizard::linux::test(self.tx, "Test the Application"),
+    Msg::DrawLinuxCompress => wizard::linux::compress(self.tx, "Compress the Created Package"),
     // Wine
-    //
-    Msg::DrawWineName =>
-    {
-      wizard::wine::name(self.tx, "Select the Application Name");
-    }
-    Msg::DrawWineIcon =>
-    {
-      wizard::wine::icon(self.tx, "Select the Application Icon");
-    }
-    Msg::DrawWineConfigure =>
-    {
-      wizard::wine::configure(self.tx, "Configure Wine");
-    }
-    Msg::DrawWineEnvironment =>
-    {
-      wizard::wine::environment(self.tx, "Configure the Environment");
-    }
-    Msg::DrawWineRom =>
-    {
-      wizard::wine::rom(self.tx, "Install/Test the Application(s)");
-    }
-    Msg::DrawWineCompress =>
-    {
-      wizard::wine::compress(self.tx, "Compress the Created Package");
-    }
-
-    //
+    Msg::DrawWineName => wizard::wine::name(self.tx, "Select the Application Name"),
+    Msg::DrawWineIcon => wizard::wine::icon(self.tx, "Select the Application Icon"),
+    Msg::DrawWineConfigure => wizard::wine::configure(self.tx, "Configure Wine"),
+    Msg::DrawWineEnvironment => wizard::wine::environment(self.tx, "Configure the Environment"),
+    Msg::DrawWineRom => wizard::wine::rom(self.tx, "Install/Test the Application(s)"),
+    Msg::DrawWineCompress => wizard::wine::compress(self.tx, "Compress the Created Package"),
     // Retroarch
-    //
-    Msg::DrawRetroarchName =>
-    {
-      wizard::retroarch::name(self.tx, "Select the Application Name");
-    }
-    Msg::DrawRetroarchIcon =>
-    {
-      wizard::retroarch::icon(self.tx, "Select the Application Icon");
-    }
-    Msg::DrawRetroarchRom =>
-    {
-      wizard::retroarch::rom(self.tx, "Install the Rom File(s)");
-    }
-    Msg::DrawRetroarchCore =>
-    {
-      wizard::retroarch::core(self.tx, "Install the Core File(s)");
-    }
-    Msg::DrawRetroarchBios =>
-    {
-      wizard::retroarch::bios(self.tx, "Install the Bios File(s)");
-    }
-    Msg::DrawRetroarchTest =>
-    {
-      wizard::retroarch::test(self.tx, "Test the Created Package");
-    }
-    Msg::DrawRetroarchCompress =>
-    {
-      wizard::retroarch::compress(self.tx, "Compress the Created Package");
-    }
-
-    //
+    Msg::DrawRetroarchName => wizard::retroarch::name(self.tx, "Select the Application Name"),
+    Msg::DrawRetroarchIcon => wizard::retroarch::icon(self.tx, "Select the Application Icon"),
+    Msg::DrawRetroarchRom => wizard::retroarch::rom(self.tx, "Install the Rom File(s)"),
+    Msg::DrawRetroarchCore => wizard::retroarch::core(self.tx, "Install the Core File(s)"),
+    Msg::DrawRetroarchBios => wizard::retroarch::bios(self.tx, "Install the Bios File(s)"),
+    Msg::DrawRetroarchTest => wizard::retroarch::test(self.tx, "Test the Created Package"),
+    Msg::DrawRetroarchCompress => wizard::retroarch::compress(self.tx, "Compress the Created Package"),
     // Pcsx2
-    //
-    Msg::DrawPcsx2Name =>
-    {
-      wizard::pcsx2::name(self.tx, "Select the Application Name");
-    }
-    Msg::DrawPcsx2Icon =>
-    {
-      wizard::pcsx2::icon(self.tx, "Select the Application Icon");
-    }
-    Msg::DrawPcsx2Rom =>
-    {
-      wizard::pcsx2::rom(self.tx, "Install the Rom File(s)");
-    }
-    Msg::DrawPcsx2Bios =>
-    {
-      wizard::pcsx2::bios(self.tx, "Install the Bios File(s)");
-    }
-    Msg::DrawPcsx2Test =>
-    {
-      wizard::pcsx2::test(self.tx, "Test the Created Package");
-    }
-    Msg::DrawPcsx2Compress =>
-    {
-      wizard::pcsx2::compress(self.tx, "Compress the Created Package");
-    }
-
-    //
+    Msg::DrawPcsx2Name => wizard::pcsx2::name(self.tx, "Select the Application Name"),
+    Msg::DrawPcsx2Icon => wizard::pcsx2::icon(self.tx, "Select the Application Icon"),
+    Msg::DrawPcsx2Rom => wizard::pcsx2::rom(self.tx, "Install the Rom File(s)"),
+    Msg::DrawPcsx2Bios => wizard::pcsx2::bios(self.tx, "Install the Bios File(s)"),
+    Msg::DrawPcsx2Test => wizard::pcsx2::test(self.tx, "Test the Created Package"),
+    Msg::DrawPcsx2Compress => wizard::pcsx2::compress(self.tx, "Compress the Created Package"),
     // Rpcs3
-    //
-    Msg::DrawRpcs3Name =>
-    {
-      wizard::rpcs3::name(self.tx, "Select the Application Name");
-    }
-    Msg::DrawRpcs3Icon =>
-    {
-      wizard::rpcs3::icon(self.tx, "Select the Application Icon");
-    }
-    Msg::DrawRpcs3Rom =>
-    {
-      wizard::rpcs3::rom(self.tx, "Install the Rom Directory(ies)");
-    }
-    Msg::DrawRpcs3Bios =>
-    {
-      wizard::rpcs3::bios(self.tx, "Install the Bios and DLC Files");
-    }
-    Msg::DrawRpcs3Test =>
-    {
-      wizard::rpcs3::test(self.tx, "Test the Created Package");
-    }
-    Msg::DrawRpcs3Compress =>
-    {
-      wizard::rpcs3::compress(self.tx, "Compress the Created Package");
-    }
-
-    //
+    Msg::DrawRpcs3Name => wizard::rpcs3::name(self.tx, "Select the Application Name"),
+    Msg::DrawRpcs3Icon => wizard::rpcs3::icon(self.tx, "Select the Application Icon"),
+    Msg::DrawRpcs3Rom => wizard::rpcs3::rom(self.tx, "Install the Rom Directory(ies)"),
+    Msg::DrawRpcs3Bios => wizard::rpcs3::bios(self.tx, "Install the Bios and DLC Files"),
+    Msg::DrawRpcs3Test => wizard::rpcs3::test(self.tx, "Test the Created Package"),
+    Msg::DrawRpcs3Compress => wizard::rpcs3::compress(self.tx, "Compress the Created Package"),
     // Quit
-    //
     Msg::Quit =>
     {
       app::quit();
