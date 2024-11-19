aa397b023a26369f93f6a351a816a03407b94bf9
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-11-19 03:10:25 -0300

    Copy individual configuration directories for emulators

--- src/cmd/package.hpp
+++ src/cmd/package.hpp
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <filesystem>
+#include <set>
 
 #include "../lib/subprocess.hpp"
 #include "../lib/db/build.hpp"
@@ -26,17 +27,43 @@ decltype(auto) portal(Args&&... args)
     .wait();
 } // portal() }}}
 
-// package_platforms() {{{
-inline void package_platforms(std::vector<std::string> const& vec_project, ns_db::ns_build::Build& db_build)
+// package_config() {{{
+inline void package_config(fs::path const& path_dir_home_src
+  , fs::path const& path_dir_home_dst
+  , std::set<ns_enum::Platform> const& set_platforms)
 {
-  // Get unique platform list
-  auto vec_platforms = db_build.projects
-    | std::views::filter([&](ns_db::ns_build::Metadata const& e){ return std::ranges::contains(vec_project, e.name); })
-    | std::views::transform([](ns_db::ns_build::Metadata const& e){ return e.platform; })
-    | std::ranges::to<std::vector<ns_enum::Platform>>();
-  std::ranges::sort_unique(vec_platforms);
-  // Include each platform in the image
-  for(auto&& platform : vec_platforms)
+  auto f_copy_recursive = [](fs::path const& path_dir_src, fs::path const& path_dir_dst)
+  {
+    for(auto&& path_file_src : fs::recursive_directory_iterator(path_dir_src)
+      | std::views::filter([](auto&& e){ return not fs::is_directory(e); })
+      | std::views::transform([](auto&& e){ return e.path(); }))
+    {
+      fs::path path_file_dst = path_dir_dst / fs::relative(path_file_src, path_dir_src);
+      ns_log::exception([&]{ fs::create_directories(path_file_dst.parent_path()); });
+      ns_log::exception([&]{ fs::copy(path_file_src, path_file_dst, fs::copy_options::overwrite_existing | fs::copy_options::copy_symlinks); });
+    } // for
+  };
+
+  if ( set_platforms.contains(ns_enum::Platform::RPCS3) )
+  {
+    f_copy_recursive(path_dir_home_src / ".config/rpcs3", path_dir_home_dst / ".config/rpcs3");
+  } // if
+
+  if ( set_platforms.contains(ns_enum::Platform::PCSX2) )
+  {
+    f_copy_recursive(path_dir_home_src / ".config/PCSX2", path_dir_home_dst / ".config/PCSX2");
+  } // if
+
+  if ( set_platforms.contains(ns_enum::Platform::RETROARCH) )
+  {
+    f_copy_recursive(path_dir_home_src / ".config/retroarch", path_dir_home_dst / ".config/retroarch");
+  } // if
+} // package_config() }}}
+
+// package_platforms() {{{
+inline void package_platforms(std::set<ns_enum::Platform> const& set_platforms, ns_db::ns_build::Build& db_build)
+{
+  for(auto&& platform : set_platforms)
   {
     fs::path path_file_layer = db_build.path_dir_cache / "{}.layer"_fmt(ns_enum::to_string_lower(platform));
     portal(db_build.path_file_output, "fim-layer", "add", path_file_layer);
@@ -78,32 +105,33 @@ inline void package(std::string const& str_name, std::string const& str_projects
     , fs::copy_options::overwrite_existing
   );
 
+  // Get list of projects
+  auto vec_project = ns_vector::from_string(str_projects, ':');
+
+  // Get list of platforms
+  std::set<ns_enum::Platform> set_platforms = db_build->projects
+    | std::views::filter([&](ns_db::ns_build::Metadata const& e){ return std::ranges::contains(vec_project, e.name); })
+    | std::views::transform([](ns_db::ns_build::Metadata const& e){ return e.platform; })
+    | std::ranges::to<std::set<ns_enum::Platform>>();
+
   // Create path to target image
   db_build->path_file_output = db_build->path_dir_build.parent_path() / (str_name + ".flatimage");
   ns_db::ns_build::write(*db_build);
 
-  // Copy data directory
-  fs::path path_dir_config_src = db_build->path_file_image.parent_path()
-    / (std::string{"."} + db_build->path_file_image.filename().string() + std::string{".config"});
-  fs::path path_dir_config_dst = db_build->path_file_output.parent_path()
-    / (std::string{"."} + db_build->path_file_output.filename().string() + std::string{".config"});
-  ns_log::write('i', "Copy ", path_dir_config_src, " to ", path_dir_config_dst);
-  for(auto&& path_file_src : fs::recursive_directory_iterator(path_dir_config_src)
-    | std::views::filter([](auto&& e){ return not fs::is_directory(e); })
-    | std::views::transform([](auto&& e){ return e.path(); }))
-  {
-    fs::path path_file_dst = path_dir_config_dst / fs::relative(path_file_src, path_dir_config_src);
-    ns_log::exception([&]{ fs::create_directories(path_file_dst.parent_path()); });
-    ns_log::exception([&]{ fs::copy(path_file_src, path_file_dst, fs::copy_options::overwrite_existing | fs::copy_options::copy_symlinks); });
-  } // for
+  // Package configuration files
+  fs::path path_dir_home_src = db_build->path_file_image.parent_path()
+    / (std::string{"."} + db_build->path_file_image.filename().string() + std::string{".config"})
+    / "overlays/upperdir/home/gameimage";
+  fs::path path_dir_home_dst = db_build->path_file_output.parent_path()
+    / (std::string{"."} + db_build->path_file_output.filename().string() + std::string{".config"})
+    / "overlays/upperdir/home/gameimage";
+  package_config(path_dir_home_src, path_dir_home_dst, set_platforms);
 
   // Copy image to output location
   fs::copy_file(db_build->path_file_image, db_build->path_file_output, fs::copy_options::overwrite_existing);
 
-  auto vec_project = ns_vector::from_string(str_projects, ':');
-
   // Include platforms
-  package_platforms(vec_project, *db_build);
+  package_platforms(set_platforms, *db_build);
 
   // Include projects
   package_projects(vec_project, *db_build);
