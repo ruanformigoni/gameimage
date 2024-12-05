eab910a38a579151e4544ead2e0dd689f7e190c4
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-12-05 07:34:20 -0300

    Builder macros for compress.rs

--- gui/wizard/src/wizard/compress.rs
+++ gui/wizard/src/wizard/compress.rs
@@ -9,7 +9,6 @@ use fltk::
 };
 
 use shared::fltk::SenderExt;
-use shared::fltk::WidgetExtExtra;
 use shared::std::PathBufExt;
 
 use crate::dimm;
@@ -19,6 +18,30 @@ use crate::common;
 use crate::log;
 use crate::log_alert;
 use crate::log_err;
+use shared::{column,row,fixed};
+
+// fn compress_next() {{{
+pub fn compress_next(tx: Sender<common::Msg>, term: frame::term::Term)
+{
+  tx.send_awake(common::Msg::WindDeactivate);
+  let backend = match gameimage::gameimage::binary()
+  {
+    Ok(backend) => backend,
+    Err(e) => { log_alert!("Error to execute backend: {}", e); return; }
+  };
+  let mut term = term.clone();
+  std::thread::spawn(move ||
+  {
+    let handle = term.dispatch(vec![&backend.string(), "compress"], |_| {});
+    match handle
+    {
+      Ok(handle) => log_err!(handle.lock().unwrap().wait().map(|_|{})),
+      Err(e) => log!("{}", e),
+    };
+    tx.send_activate(common::Msg::DrawCreator);
+  });
+
+} // fn compress_next() }}}
 
 // pub fn compress() {{{
 pub fn compress(tx: Sender<common::Msg>
@@ -28,41 +51,36 @@ pub fn compress(tx: Sender<common::Msg>
   , _msg_next: common::Msg)
 {
   let ui = crate::GUI.lock().unwrap().ui.clone()(title);
+  // Layout
+  column!(col,
+    let term = frame::term::Term::default();
+    col.add(&term.group);
+    row!(row,
+      fixed!(row, btn_level, menu::MenuButton::default(), dimm::width_button_wide());
+      row.add(&fltk::frame::Frame::default()
+        .with_align(fltk::enums::Align::Inside | fltk::enums::Align::Center)
+        .with_label("Select the compression level before clicking on start")
+      );
+    );
+    col.fixed(&row, dimm::height_button_wide());
+  );
 
-  // Set previous frame
+  // Configure buttons
   ui.btn_prev.clone().emit(tx.clone(), msg_prev);
-
-  // Rename 'next' to 'compress'
-  ui.btn_next.clone().set_label("Start");
-
-  let mut term = frame::term::Term::new(dimm::border()
-    , ui.group.w()
-    , ui.group.h()
-    , ui.group.x()
-    , ui.group.y());
+  let mut btn_next = ui.btn_next.clone();
+  btn_next.set_label("Start");
+  btn_next.set_callback(move |_| { compress_next(tx, term.clone()); });
 
   // Open space for compress level button
-  let mut widget_term = term.term.clone();
-  widget_term.set_size(term.term.w()
-    , term.term.h() - dimm::border() - dimm::height_button_wide()
-  );
-  let mut btn_level = menu::MenuButton::default()
-    .with_size(dimm::width_button_wide(), dimm::height_button_wide())
-    .with_focus(false)
-    .bottom_left_of(&ui.group, 0)
-    .with_callback(|e|
-    {
-      let str_level = e.choice().unwrap_or(String::from("7"));
-      log!("Set compression level to {}", str_level);
-      env::set_var("FIM_COMPRESSION_LEVEL", &str_level);
-      e.set_value(e.value());
-      e.set_label(&str_level);
-    });
-  let _ = fltk::frame::Frame::default()
-    .with_size(widget_term.w() - dimm::width_button_wide(), dimm::height_text())
-    .with_pos(btn_level.x() + btn_level.w() + dimm::border(), btn_level.y() + btn_level.h() / 4)
-    .with_align(fltk::enums::Align::Inside | fltk::enums::Align::Center)
-    .with_label("Select the compression level before clicking on start");
+  let mut btn_level = btn_level.clone();
+  btn_level.set_callback(|e|
+  {
+    let str_level = e.choice().unwrap_or(String::from("7"));
+    log!("Set compression level to {}", str_level);
+    env::set_var("FIM_COMPRESSION_LEVEL", &str_level);
+    e.set_value(e.value());
+    e.set_label(&str_level);
+  });
   for i in 0..11
   {
     btn_level.add_choice(&i.to_string());
@@ -70,28 +88,6 @@ pub fn compress(tx: Sender<common::Msg>
   env::set_var("FIM_COMPRESSION_LEVEL", "7");
   btn_level.set_value(8);
   btn_level.set_label("7");
-
-  let clone_tx = tx.clone();
-  ui.btn_next.clone().set_callback(move |_|
-  {
-    clone_tx.send_awake(common::Msg::WindDeactivate);
-    let backend = match gameimage::gameimage::binary()
-    {
-      Ok(backend) => backend,
-      Err(e) => { log_alert!("Error to execute backend: {}", e); return; }
-    };
-    let mut term = term.clone();
-    std::thread::spawn(move ||
-    {
-      let handle = term.dispatch(vec![&backend.string(), "compress"], |_| {});
-      match handle
-      {
-        Ok(handle) => log_err!(handle.lock().unwrap().wait().map(|_|{})),
-        Err(e) => log!("{}", e),
-      };
-      clone_tx.send_activate(common::Msg::DrawCreator);
-    });
-  });
 } // fn compress() }}}
 
 // vim: set expandtab fdm=marker ts=2 sw=2 tw=100 et :
