b6f293c40bed8df6b29edf3d5bf9708f0dd39c00
Ruan E. Formigoni <ruanformigoni@gmail.com>
2024-12-14 14:32:14 -0300

    Builder macros for rpcs3.rs

--- gui/wizard/src/wizard/rpcs3.rs
+++ gui/wizard/src/wizard/rpcs3.rs
@@ -11,6 +11,7 @@ use fltk::
   prelude::*,
 };
 
+use shared::{hover_blink,column,row,add,fixed};
 use shared::fltk::WidgetExtExtra;
 use shared::fltk::SenderExt;
 use shared::dimm;
@@ -46,29 +47,32 @@ pub fn icon(tx: Sender<common::Msg>, title: &str)
 pub fn rom(tx: Sender<common::Msg>, title: &str)
 {
   let ui = crate::GUI.lock().unwrap().ui.clone()(title);
-
-  // Set previous frame
+  // Layout
+  row!(row,
+    add!(row, frame_list, MultiBrowser::default());
+    column!(col,
+      fixed!(col, btn_add, shared::fltk::button::rect::add(), dimm::height_button_rec());
+      fixed!(col, btn_del, shared::fltk::button::rect::del(), dimm::height_button_rec());
+    );
+    row.fixed(&col, dimm::width_button_rec());
+  );
+  // Configure buttons
   ui.btn_prev.clone().emit(tx.clone(), common::Msg::DrawRpcs3Icon);
   ui.btn_next.clone().emit(tx.clone(), common::Msg::DrawRpcs3Bios);
-
   // List of the currently installed items
-  let mut frame_list = MultiBrowser::default()
-    .with_size(ui.group.width() - dimm::border() - dimm::width_button_rec(), ui.group.height())
-    .with_pos_of(&ui.group);
+  let mut frame_list = frame_list.clone();
   frame_list.set_frame(FrameType::BorderBox);
   frame_list.set_text_size(dimm::height_text());
-
   // Insert items in list of currently installed items
   let result_vec_items = gameimage::search::search_local("rom");
   if let Ok(vec_items) = result_vec_items
   {
     for item in vec_items { frame_list.add(&item.string()); } // for
   } // if
-
   // Add new item
-  let mut btn_add = shared::fltk::button::rect::add()
-    .right_of(&frame_list, dimm::border())
+  let mut btn_add = btn_add.clone()
     .with_color(Color::Green);
+  hover_blink!(btn_add);
   let clone_tx = tx.clone();
   btn_add.set_callback(move |_|
   {
@@ -77,26 +81,20 @@ pub fn rom(tx: Sender<common::Msg>, title: &str)
       , "*"
       , dialog::FileChooserType::Directory
       , "Pick a directory with the .SFB file");
-
     // Start dialog
     chooser.show();
-
     // Wait for choice(s)
     while chooser.shown() { fltk::app::wait(); } // while
-
     // Check if choice is valid
     if chooser.value(1).is_none()
     {
       log_status!("No file selected");
       return;
     } // if
-
     // Deactivate window
     clone_tx.send_awake(common::Msg::WindDeactivate);
-
     // Fetch choice
     let str_choice = chooser.value(1).unwrap();
-
     // Install
     let clone_tx = clone_tx.clone();
     std::thread::spawn(move ||
@@ -110,31 +108,27 @@ pub fn rom(tx: Sender<common::Msg>, title: &str)
       clone_tx.send_activate(common::Msg::DrawRpcs3Rom);
     });
   });
-
   // Erase package
-  let mut btn_del = shared::fltk::button::rect::del()
-    .below_of(&btn_add, dimm::border())
+  let mut btn_del = btn_del.clone()
     .with_color(Color::Red);
+  hover_blink!(btn_del);
   btn_del.set_callback(move |_|
   {
     let vec_indices = frame_list.selected_items();
-
+    // Check if any item was selected
     if vec_indices.len() == 0
     {
       log_status!("No item selected for deletion");
       return;
     } // if
-
     // Get items
     let vec_items : Vec<String> = vec_indices.into_iter().map(|e|{ frame_list.text(e).unwrap() }).collect();
-
     // Run backend
     match gameimage::install::remove("rom", vec_items)
     {
       Ok(_) => log_status!("Removed rom(s) successfully"),
       Err(e) => log_status!("Could not remove rom(s): '{}'", e),
     } // match
-
     clone_tx.send_activate(common::Msg::DrawRpcs3Rom);
   });
 } // }}}
@@ -143,44 +137,46 @@ pub fn rom(tx: Sender<common::Msg>, title: &str)
 pub fn bios(tx: Sender<common::Msg>, title: &str)
 {
   let ui = crate::GUI.lock().unwrap().ui.clone()(title);
-
-  // Set bottom callbacks
+  // Layout
+  column!(col,
+    add!(col, frame_text, output::MultilineOutput::default());
+    row!(row,
+      row.add(&fltk::frame::Frame::default());
+      fixed!(row, btn_launch, shared::fltk::button::wide::default(), dimm::width_button_wide());
+      row.add(&fltk::frame::Frame::default());
+    );
+    col.fixed(&row, dimm::height_button_wide());
+  );
+  // Set button callbacks
   ui.btn_prev.clone().emit(tx.clone(), common::Msg::DrawRpcs3Rom);
   ui.btn_next.clone().emit(tx.clone(), common::Msg::DrawRpcs3Test);
-
   // Box with explanation text
-  let mut frame_text = output::MultilineOutput::default()
-    .with_size(ui.group.w(), ui.group.h() - dimm::border() - dimm::height_button_wide())
-    .with_pos_of(&ui.group);
+  let mut frame_text = frame_text.clone();
   frame_text.set_color(Color::BackGround);
   frame_text.set_frame(FrameType::BorderBox);
   frame_text.set_text_size(dimm::height_text());
-
   let _ = frame_text.append("Here you can install the firmware and the .pkg, .rap and .edat files\n");
   let _ = frame_text.append("Clicking on 'Open' will open RPCS3\n");
   let _ = frame_text.append("Go to 'File -> Install Packages/Raps/Edats' for DLC\n");
   let _ = frame_text.append("Go to 'File -> Install Firmware' for the BIOS\n");
-
   // Button to launch rpcs3 and install files
-  let _btn_launch = shared::fltk::button::wide::default()
-    .below_center_of(&frame_text, dimm::border())
-    .with_color(Color::Green)
-    .with_label("Open")
-    .with_callback(move |_|
+  let mut btn_launch = btn_launch;
+  btn_launch.set_color(Color::Green);
+  btn_launch.set_label("Open");
+  btn_launch.set_callback(move |_|
+  {
+    tx.send_awake(common::Msg::WindDeactivate);
+    std::thread::spawn(move ||
     {
-      tx.send_awake(common::Msg::WindDeactivate);
-      std::thread::spawn(move ||
+      match gameimage::install::gui()
       {
-        match gameimage::install::gui()
-        {
-          Ok(_) => log_status!("Gui exited successfully"),
-          Err(e) => log_status!("Install gui exited with error: {}", e),
-        }; // match
-
-        tx.send_awake(common::Msg::WindActivate);
-      });
+        Ok(_) => log_status!("Gui exited successfully"),
+        Err(e) => log_status!("Install gui exited with error: {}", e),
+      }; // match
+      tx.send_awake(common::Msg::WindActivate);
     });
-
+  });
+  hover_blink!(btn_launch);
 } // }}}
 
 // pub fn test() {{{
